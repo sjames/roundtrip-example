166a0b1238c61b49a6a64db9a4ac09ecd4ac1dae
Sojan James <sojan.james@gmail.com>
2020-09-27 18:14:34 +0530

    Works with cycloneDDS roundtripping example.
    There is still a major bug. The memory allocated by cyclonedds for
    rx is not yet free.

--- roundtrip-data/src/lib.rs
+++ roundtrip-data/src/lib.rs
@@ -3,8 +3,12 @@
 #![allow(non_snake_case)]
 
 use std::env;
+#[cfg(not(feature = "rust_codegen"))]
+use cyclonedds_sys::DDSGenType;
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
+#[cfg(not(feature = "rust_codegen"))]
+include!(concat!(env!("OUT_DIR"), "/DdsAllocator_impl.rs"));
 
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -3,47 +3,39 @@
 use cyclonedds_rs::dds_reliability_kind;
 use cyclonedds_rs::{
     dds_attach_t, dds_duration_t, DdsPublisher, DdsQos, DdsReader, DdsSubscriber, DdsTopic,
-    DdsWriter, DdsWaitset, DdsReadCondition, dds_api::StateMask, dds_api::State, dds_triggered,
+    DdsWriter, DdsWaitset, DdsReadCondition, dds_api::StateMask, dds_api::State, dds_triggered, take, DdsLoanedData,
 };
 
 use roundtrip_data::RoundTripModule::DataType;
+use roundtrip_data::*;
 fn main() {
     println!("Roundtrip Pong!");
-
     let wait_timeout: dds_duration_t = std::i64::MAX;
-
     if let Ok(participant) = cyclonedds_rs::DdsParticipant::create(None, None, None) {
-        
-        let listener = cyclonedds_rs::DdsListener::new()
-            .on_data_available(|entity| {
-                println!("Data available");
-            })
-            .hook();
-
-        if let Ok(topic) = DdsTopic::<DataType>::create(&participant, "RoundTrip", None, None) {
+        if let Ok(topic) = DdsTopic::<RoundTripModule::DataType>::create(&participant, "RoundTrip", None, None) {
             let mut qos = cyclonedds_rs::DdsQos::create().unwrap();
             qos.set_partition(&std::ffi::CString::new("pong").unwrap());
 
-            let publisher = DdsPublisher::create(&participant, Some(qos), None)
+            let publisher = DdsPublisher::create(&participant, Some(&qos), None)
                 .expect("Unable to create publisher");
 
             // A Dds data writer is created on the publisher and topic
-            let mut qos = DdsQos::create().expect("Unable to create qos");
+            let mut writer_qos = DdsQos::create().expect("Unable to create qos");
             let duration = std::time::Duration::from_secs(10);
-            qos.set_reliability(
+            writer_qos.set_reliability(
                 dds_reliability_kind::DDS_RELIABILITY_RELIABLE,
                 duration.as_nanos() as i64,
             );
-            qos.set_writer_data_lifecycle(false);
+            writer_qos.set_writer_data_lifecycle(false);
 
             let mut writer =
-                DdsWriter::<DataType>::create(&publisher, &topic, Some(qos), None).unwrap();
+                DdsWriter::<RoundTripModule::DataType>::create(&publisher, &topic, Some(&writer_qos), None).unwrap();
 
             // A dds subscriber is created on the domain participant
             let mut qos = DdsQos::create().unwrap();
             qos.set_partition(&std::ffi::CString::new("ping").unwrap());
 
-            let subscriber = DdsSubscriber::create(&participant, Some(qos), None).unwrap();
+            let subscriber = DdsSubscriber::create(&participant, Some(&qos), None).unwrap();
 
             // a data reader is created on the subscriber and topic
             let mut qos = DdsQos::create().unwrap();
@@ -51,19 +43,40 @@ fn main() {
                 dds_reliability_kind::DDS_RELIABILITY_RELIABLE,
                 duration.as_nanos() as i64,
             );
-            let mut reader = DdsReader::create(&subscriber, &topic, Some(qos), Some(listener)).unwrap();
+
+            let listener = cyclonedds_rs::DdsListener::new()
+            .on_data_available(move|entity| {
+
+                //println!("Data available");
+                
+                    if let Ok(data) = unsafe {cyclonedds_rs::take::<RoundTripModule::DataType>(&entity)} {
+                        let data = data.as_slice();
+                        println!("Received data len:{}",data.len());
+
+                        for d in data {
+                            println!("Payload len:{}",d.payload._length);
+                            let ret = writer.write(&d);
+                        }
+                    } else {
+                        panic!("take failed");
+                    }
+
+            })
+            .hook();
+
+            let mut reader = DdsReader::create(&subscriber, &topic, Some(&qos), Some(listener)).unwrap();
 
             let mut waitset = DdsWaitset::<isize>::create(&participant).expect("Unable to create waitset");
             
-            let mut mask = StateMask::none();
-            mask.set(State::DdsAnyState);
-            let read_condition = reader.create_readcondition(mask).expect("Unable to create readcondition");
-            let i = 0;
-            let j = 0;
-            waitset.attach(&read_condition,&i).expect("Failed to attach waitset");
+           // let mut mask = StateMask::none();
+           // mask.set(State::DdsAnyState);
+
+            //let read_condition = reader.create_readcondition(mask).expect("Unable to create readcondition");
+            //let i = 0;
+            //let j = 0;
+            //waitset.attach(&read_condition,&i).expect("Failed to attach waitset");
             
-            //waitset.attach(&waitset,&j).expect("Failed to attack waitset waitset");
-            
+            ////waitset.attach(&waitset,&j).expect("Failed to attack waitset waitset");
 
             while dds_triggered(&waitset).is_ok() {
                 let mut xs = Vec::new();
@@ -73,13 +86,17 @@ fn main() {
                     if xs.len() == 0 {
                         
                     } else {
-                        println!("Wait returned something");
+                        //println!("Wait returned something");
                     }
 
                 } else {
                     println!("wait returned error");
                 }
 
+            }
+
+            loop {
+                panic!("Loop done");
             }
 
         } else {
